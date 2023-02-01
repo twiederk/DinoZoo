0a4e84ffd1bda54a613deb446fbaf18be1759b1c
in57kc <torsten.wiederkehr@ing.com>
2023-02-01 15:23:54 +0100

    Added movement based on velocity. Added DinoResource with speed, texture and collision shape

--- scripts/Dino.gd
+++ scripts/Dino.gd
@@ -1,58 +1,36 @@
 class_name Dino
-extends Area2D
+extends CharacterBody2D
 
-var textures = [
-	'Ankylosaurus.png',
-	'Brontosaurus.png',
-	'Spinosaurus.png',
+var speed: int = 30
+
+var dinos = [
+	preload("res://resources/Ankylosaurus.tres"),
+	preload("res://resources/Brontosaurus.tres"),
+	preload("res://resources/Spinosaurus.tres"),
 	]
 
-var tile_size = 16
-var can_move = true
-var facing = 'right'
-var moves = {
-	'right': Vector2(1, 0),
-	'left': Vector2(-1, 0),
-	'up': Vector2(0, -1),
-	'down': Vector2(0, 1)
-	}
+var direction = [
+	Vector2(1, 0),
+	Vector2(-1, 0),
+	Vector2(0, -1),
+	Vector2(0, 1)
+	]
 
-var raycasts = {
-	'right': 'RayCastRight',
-	'left': 'RayCastLeft',
-	'up': 'RayCastUp',
-	'down': 'RayCastDown'
-	}
+@onready var sprite = $Sprite
+@onready var collision_shape = $CollisionShape2D
 
 
 func _ready():
 	randomize()
-	var texture = textures[randi() % textures.size()]
-	texture = load("res://assets/%s" % texture)
-	$Sprite.texture = texture
-	facing = moves.keys()[randi() % 4]
+	var dino = dinos[randi() % dinos.size()]
+	speed = dino.speed
+	sprite.texture = dino.texture
+	collision_shape.shape = dino.collision_shape
+	velocity = direction[randi() % direction.size()] * speed
 
 
-func _process(_delta):
-	if can_move:
-		if not move(facing) or randi() % 10 > 5:
-			facing = moves.keys()[randi() % 4]
-
-
-func move(dir: String) -> bool:
-	if get_node(raycasts[facing]).is_colliding():
-		return false
-	facing = dir
-	can_move = false
-	$Timer.start()
-#	$AnimationPlayer.play(facing)
-#	$MoveTween.interpolate_property(self, "position", position,
-#		position + moves[facing] * tile_size, 0.8,
-#		Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-#	$MoveTween.start()
-	position = position + moves[facing] * tile_size
-	return true
-
-
-func _on_Timer_timeout():
-	can_move = true
+func _physics_process(_delta):
+	var collide = move_and_slide()
+	if collide:
+		velocity = direction[randi() % direction.size()] * speed
+		
---
+++ scripts/DinoResource.gd
@@ -0,0 +1,6 @@
+class_name DinoResource
+extends Resource
+
+@export var speed: int = 30
+@export var texture: Texture
+@export var collision_shape: Shape2D
